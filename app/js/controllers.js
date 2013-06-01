3d892f7444b9c546ee8eb3e2aecc624ac39e137c
Lawrence Gunn <lawrence@hookflash.com>
2013-06-01 13:44:28 -0700

    Sprint 1: Added Bootstrap. Toolbar and base timing waveform editor.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -9,3 +9,10 @@ angular.module('myApp.controllers', []).
   .controller('MyCtrl2', [function() {
 
   }]);
+
+function HeaderController($scope) {
+  $scope.blah = "Hi there";
+}
+
+function TimingEditorController($scope) {
+}
