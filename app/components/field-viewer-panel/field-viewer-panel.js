346e823f283c798acc6b1b7227f8aa6e26e784df
Lawrence Gunn <lawrencegunn@gmail.com>
2014-09-01 11:12:33 -0700

    Spike 1: Added rotational viewer.

--- app/components/field-viewer-panel/field-viewer-panel.js
+++ app/components/field-viewer-panel/field-viewer-panel.js
@@ -19,35 +19,61 @@ angular.module('nSimApp.directives').directive('fieldViewerPanel', [function() {
     controller: ['ComponentExtensions', '$scope', '$element', '$attrs', '$timeout', function (ComponentExtensions, $scope, $element, $attrs, $timeout) {
       ComponentExtensions.initialize(this, 'fieldViewerPanel', $scope, $attrs);
 
+      $scope.stopRendering = false;
 
     }],
-    link: function($scope, $element, $attrs) {
+    link: function($scope, $element, $attrs, ctrl) {
+
+      $scope.stop = function() {
+        if($scope.stopRendering) {
+          $scope.stopRendering = false;
+        } else {
+          $scope.stopRendering = true;
+        }
+      }
 
       var initializeRenderer = function() {
+        if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
+
         var target = $element.find('.render-target');
 
         var scene = new THREE.Scene();
         var camera = new THREE.PerspectiveCamera( 75, target.width() / target.height(), 0.1, 1000 );
+
+        var controls = new THREE.OrbitControls( camera );
+        controls.damping = 0.2;
 
         var renderer = new THREE.WebGLRenderer();
         renderer.setSize( target.width(), target.height() );
         target.append(renderer.domElement);
 
         var geometry = new THREE.BoxGeometry(1,1,1);
-        var material = new THREE.MeshBasicMaterial({ color: 0x008800 });
+        var material = new THREE.MeshPhongMaterial({ specular: '#a9fcff',
+          // intermediate
+          color: '#00abb1',
+          // dark
+          emissive: '#006063',
+          shininess: 100  });
+
         var cube = new THREE.Mesh(geometry, material);
         scene.add(cube);
 
         camera.position.z = 3;
 
+        // add subtle ambient lighting
+        var ambientLight = new THREE.AmbientLight(0x222222);
+        scene.add(ambientLight);
+
+        // directional lighting
+        var directionalLight = new THREE.DirectionalLight(0xffffff);
+        directionalLight.position.set(1, 1, 1).normalize();
+        scene.add(directionalLight);
+
         var render = function () {
           requestAnimationFrame(render);
-
-          cube.rotation.x += 0.01;
-          cube.rotation.y += 0.01;
-
           renderer.render(scene, camera);
         };
+        controls.addEventListener( 'change', render );
 
         render();
 
